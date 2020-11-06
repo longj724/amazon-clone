220d99b14f1ebb19504534da8a02c48812ff961b
KindaOK <ondrej5385@gmail.com>
2020-11-05 19:31:18 -0500

    Making images on ProductCard work
    fixes the image issues that product cards were having and adds the link to the card page.

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles({
     root: {
@@ -24,27 +25,29 @@ function ProductCard(props) {
     const url = `/?id=${props.id}`
     const details = props.details
     const image_url = props.image_id
-    const base_url = ""
+    const base_url = "https://placeimg.com/640/480/"
 
     return (
         <Card className={classes.root}>
-            <CardActionArea href={url}>
-                <CardMedia
-                    className={classes.media}
-                    component="img"
-                    alt={name}
-                    image={base_url + image_url}
-                    title={name}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {name}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {details}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+                {/*This part adds the link to the component without changing the internal styling*/}
+                <CardActionArea component={Link} to={url}>
+                        <CardMedia
+                            className={classes.media}
+                            component="img"
+                            alt={name}
+                            image={base_url + id}
+                            title={name}
+                        />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {details}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+
         </Card>
     );
 }
