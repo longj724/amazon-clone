3f45031402aa39ddc8d8f61af92e87830782d61f
juslong <juslong@umich.edu>
2021-02-12 23:12:29 -0500

    cleaning up code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './css/App.css';
 import MenuBar from './components/MenuBar.jsx';
 import ProductCard from './components/ProductCard';
